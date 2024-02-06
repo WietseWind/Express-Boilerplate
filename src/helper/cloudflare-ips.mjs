962d3d3166d7d4136e4ab3d1e652b5f1b2bb1ed5
Wietse Wind <mail@wietse.com>
2024-02-06 03:35:12 +0100

    Update cloudflare-ips.mjs

--- src/helper/cloudflare-ips.mjs
+++ src/helper/cloudflare-ips.mjs
@@ -1,5 +1,6 @@
 import fetch from 'node-fetch'
 
+// I know, I know, ugliest piece of shit. But works pretty damn well.
 const cloudflareIps = [ ...await Promise.all((await Promise.all([4, 6].map(v => fetch('https://www.cloudflare.com/ips-v' + v)))).map(async r => (await r.text()).trim().split(`\n`))) ].flat()
 
 export {
